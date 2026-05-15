a60e9401dcca5e3a84bafe68c404d00921d1ad69
EmmanuelM-A <madukaagbeze.ea@gmail.com>
2026-05-15 06:31:14 +0100

    feat(chatbot_factory): refactor chatbot construction to use factory functions for processors and repositories

--- src/components/chatbot/chatbot_factory.py
+++ src/components/chatbot/chatbot_factory.py
@@ -6,11 +6,14 @@ from typing import Optional
 
 from src.components.chatbot.core import RAGChatbot
 from src.components.chatbot.query_handler import QueryHandler
-from src.components.ingestion.document_processor import DocumentProcessor
+from src.components.ingestion.processor_factory import (
+    get_vector_processor,
+    get_upload_document_processor,
+)
 from src.components.retrieval.embedder import Embedder
-from src.components.retrieval.faiss_store import FaissVectorStore
-from src.components.retrieval.vector_store import VectorStore
 from src.components.retrieval.web_searcher import WebSearcher
+from src.database.repository import get_database_repository
+from src.database.repository.database_repository_factory import get_tx_factory
 
 # Singleton instance of RAGChatbot (USE FACTORY METHOD TO ACCESS)
 _rag_chatbot_instance: Optional[RAGChatbot] = None
@@ -18,22 +21,22 @@ _rag_chatbot_instance: Optional[RAGChatbot] = None
 
 def _build_chatbot() -> RAGChatbot:
     """Construct a fully wired RAGChatbot instance lazily."""
-    document_processor: DocumentProcessor = DocumentProcessor()
-    vector_store: VectorStore = FaissVectorStore()
     embedder: Embedder = Embedder()
-    query_handler: QueryHandler = QueryHandler(embedder=embedder)
+    query_handler: QueryHandler = QueryHandler(
+        embedder=embedder, document_chunk_repo=get_database_repository("DOCUMENT_CHUNK")
+    )
     web_searcher: WebSearcher = WebSearcher(
         embedder=embedder,
-        document_processor=document_processor,
-        vector_store=vector_store,
+        vector_processor=get_vector_processor(),
     )
 
     return RAGChatbot(
-        vector_store=vector_store,
-        document_processor=document_processor,
+        document_processor=get_upload_document_processor(),
         embedder=embedder,
         query_handler=query_handler,
         web_searcher=web_searcher,
+        tx_factory=get_tx_factory(),
+        document_chunk_repo=get_database_repository("DOCUMENT_CHUNK"),
     )
 
 
